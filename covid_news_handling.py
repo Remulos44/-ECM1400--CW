61bb4820f5fc06d4197541504135fc4cc986498c
Joey <josefgriffiths@me.com>
2021-11-20 17:30:07 +0000

    Added to covid_news_handling module.

--- covid_news_handling.py
+++ covid_news_handling.py
@@ -1,4 +1,6 @@
 import requests
 
-def news_API_request("Covid COVID-19 coronavirus"):
+def news_API_request(covid_terms = "Covid COVID-19 coronavirus"):
     base_URL = "https://newsapi.org/v2/everything?"
+
+def update_news():
