b36b2017b9d3131496505abe15fc57f20f8b926e
Joey <josefgriffiths@me.com>
2021-11-27 16:37:04 +0000

    Added functionality to 'interface.py' to make it display the template

--- interface.py
+++ interface.py
@@ -1,3 +1,18 @@
 from flask import Flask, render_template, request
+import sched, time
+from covid_news_handling import news_API_request
 
 app = flask(__name__)
+s = sched.scheduler(time.time, time.sleep)
+news = news_API_request()
+
+@app.route('/index')
+def initial():
+    return render_template(
+        'index.html',
+        title = 'Daily Update,
+        news_articles = news'
+        )
+
+if __name__ == "__main__":
+    app.run()
