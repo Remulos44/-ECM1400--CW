6dff3d59a5ad857866fe40b72a4e75d47afbb124
Joey <josefgriffiths@me.com>
2021-12-01 12:31:07 +0000

    edited covid_data_handler module to work with data not from a file

--- covid_data_handler.py
+++ covid_data_handler.py
@@ -1,6 +1,8 @@
 import datetime, json, sched, time
 from uk_covid19 import Cov19API
 
+s = sched.scheduler(time.time, time.sleep)
+
 def parse_csv_data(csv_filename: str) -> list:
     data = []
     f = open(csv_filename)
@@ -40,14 +42,13 @@ def covid_API_request(location = "Exeter", location_type = "ltla"):
         "new_cases": "newCasesBySpecimenDate"
     }
     api = Cov19API(filters=location_filter, structure=structure_filter)
-    data = api.get_json(save_as="./data.json")
+    data = api.get_json()
     return data
 
-def process_covid_json_data(file_name):
+def process_covid_json_data():
     today = datetime.datetime.today()
-    f = open("data.json", "r")
-    file = json.load(f)
-    data = file["data"]
+    all_data = covid_API_request()
+    data = all_data['data']
     cases_last_7_days = 0
     current_hospital_cases = data[0]["hospital_cases"]
     total_deaths = data[0]["cummulative_deaths"]
@@ -55,10 +56,11 @@ def process_covid_json_data(file_name):
         date = datetime.datetime.strptime(line["date"], "%Y-%m-%d")
         if date >= today - datetime.timedelta(days=7):
             cases_last_7_days += line["new_cases"]
-    f.close()
     return cases_last_7_days, current_hospital_cases, total_deaths
 
+def update_covid_data():
+    schedule_covid_updates(60*60*24, covid_API_request())
+
 def schedule_covid_updates(update_interval, update_name):
-    s = sched.scheduler(time.time, time.sleep)
-    e1 = s.enter(update_interval,1,update_name,,))
-    s.run()
+    e1 = s.enter(update_interval,1,update_name)
+    s.run(blocking=False)
