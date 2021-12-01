d07b1c45d667f19de5c765b866d71b12a47aaef4
Joey <josefgriffiths@me.com>
2021-12-01 12:29:22 +0000

    Passed through variables to the site interface

--- interface.py
+++ interface.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template, request
-import sched, time
+import sched, time, json
 from covid_news_handling import news_API_request
+from covid_data_handler import process_covid_json_data
 
-app = flask(__name__)
+app = Flask(__name__)
 s = sched.scheduler(time.time, time.sleep)
 news = news_API_request()
 
+@app.route('/')
+@app.route('/home')
 @app.route('/index')
 def initial():
+    cases_last_7_days, current_hospital_cases, total_deaths = process_covid_json_data()
     return render_template(
         'index.html',
-        title = 'Daily Update,
-        news_articles = news'
+        title = 'Daily Covid Update',
+        location = 'Exeter',
+        nation_location = 'UK',
+        news_articles = news['articles'],
+        image = 'covid19_image.jpg',
+        local_7day_infections = cases_last_7_days,
+        hospital_cases = current_hospital_cases,
+        deaths_total = total_deaths
         )
 
 if __name__ == "__main__":
