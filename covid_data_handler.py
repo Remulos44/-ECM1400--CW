994cc05de9427843932e245f7fe7867d11219c46
Joey <josefgriffiths@me.com>
2021-12-06 12:00:44 +0000

    Improved layout in accordance with pylint

--- covid_data_handler.py
+++ covid_data_handler.py
@@ -1,17 +1,28 @@
-import datetime, json, sched, time
+"""
+Module for handling covid data
+"""
+
+import datetime
+import sched
+import time
 from uk_covid19 import Cov19API
 
 s = sched.scheduler(time.time, time.sleep)
 
 def parse_csv_data(csv_filename: str) -> list:
+    """
+    Function to read 'nation_2021-10-28.csv' file and return its data
+    """
     data = []
-    f = open(csv_filename)
-    for line in f:
-        data.append(line)
-    f.close()
+    with open(csv_filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            data.append(line)
     return data
 
 def process_covid_csv_data(covid_csv_data: list):
+    """
+    Function to process the data retrieved by the 'parse_csv_data' function
+    """
     del covid_csv_data[0]
     cases_last_7_days = 0
     current_hospital_cases = int(covid_csv_data[0].split(",")[5])
@@ -23,11 +34,15 @@ def process_covid_csv_data(covid_csv_data: list):
             if total_deaths < int(split_line[4]):
                 total_deaths = int(split_line[4])
         date = datetime.datetime.strptime(split_line[3], "%Y-%m-%d")
-        if date >= (date_now - datetime.timedelta(days=7)) and date < date_now:
+        #if date >= (date_now - datetime.timedelta(days=7)) and date < date_now:
+        if (date_now - datetime.timedelta(days=7)) <= date < date_now:
             cases_last_7_days += int(split_line[6])
     return cases_last_7_days, current_hospital_cases, total_deaths
 
 def covid_API_request(location = "Exeter", location_type = "ltla"):
+    """
+    Function to retrieve covid date from Cov19API
+    """
     location_filter = [
         'areaType=' + location_type,
         'areaName=' + location
@@ -46,6 +61,9 @@ def covid_API_request(location = "Exeter", location_type = "ltla"):
     return data
 
 def process_covid_json_data():
+    """
+    Function to extract the data required from the data retrieved by the covid_API_request function
+    """
     today = datetime.datetime.today()
     all_data = covid_API_request()
     data = all_data['data']
@@ -58,9 +76,7 @@ def process_covid_json_data():
             cases_last_7_days += line["new_cases"]
     return cases_last_7_days, current_hospital_cases, total_deaths
 
-def update_covid_data():
-    schedule_covid_updates(60*60*24, covid_API_request())
-
 def schedule_covid_updates(update_interval, update_name):
-    e1 = s.enter(update_interval,1,update_name)
-    s.run(blocking=False)
+    """
+    Function to schedual covid data updates
+    """    
--- covid_news_handling.py
+++ covid_news_handling.py
@@ -1,21 +1,32 @@
-from covid_data_handler import schedule_covid_updates
-import requests, json
+"""
+Module for handling covid news articles
+"""
 
-def news_API_request(search_terms = "Covid COVID-19 coronavirus"):
-    base_URL = "https://newsapi.org/v2/everything?"
+import json
+import requests
+from covid_data_handler import schedule_covid_updates
+
+
+def news_API_request(search_terms="Covid COVID-19 coronavirus"):
+    """
+    Function for retrieving news articles from the 'newsapi.org' api
+    """
+    base_url = "https://newsapi.org/v2/everything?"
     mentioning = search_terms.lower() + "&"
 
-    f = open("config.json", "r")
-    file = json.load(f)
-    api_key = file["api_key"]
-    f.close()
+    with open("config.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+        api_key = data["api_key"]
 
-    final_URL = base_URL + "q=" + mentioning + "apikey=" + api_key
-    response = requests.get(final_URL)
+    final_url = base_url + "q=" + mentioning + "apikey=" + api_key
+    response = requests.get(final_url)
     return response.json()
 
+
 def filtered_news(removed_articles):
-    #Removes articles that have been deleted by the user
+    """
+    Function for removing articles from the list
+    """
     news = news_API_request()
     for article_title in removed_articles:
         for article in news['articles']:
@@ -24,8 +35,13 @@ def filtered_news(removed_articles):
                 del news['articles'][index]
     return news['articles']
 
+
 def update_news():
+    """
+    Function for updating the news list
+    """
     schedule_covid_updates(60**2, news_API_request())
+
 
 if __name__ == "__main__":
     news_API_request()
